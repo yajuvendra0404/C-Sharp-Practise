c6c2ca68b2c8e1bda1bacffa3bd6670ccf901691
Yajuvendra-Thakur <Yajuvendra.Thakur@flemingcollege.ca>
2026-03-14 14:19:50 -0400

    simple calculations

---
+++ Practise-1/Practise-1/Operations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_1
+{
+    internal class Operation
+    {
+        private int a, b, c;
+        internal Operation(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        internal void add()
+        {
+            Console.WriteLine("addition" + (a + b));
+        }
+        internal void subtract()
+        {
+            Console.WriteLine("subtraction" + (b - a));
+        }
+        internal void divide()
+        {
+            Console.WriteLine("division" + (b / a));
+        }
+        internal void multiply()
+        {
+            Console.WriteLine("multiplication" + (a * b));
+        }
+    }
+}
--- Practise-1/Practise-1/Program.cs
+++ Practise-1/Practise-1/Program.cs
@@ -10,6 +10,46 @@ namespace Practise_1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("### Calculator ###");
+
+            Console.WriteLine("Enter A = ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B = ");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(@"What operation do you want to perform?
+            A. Addition.
+            B. Subraction.
+            C. Multiplication.
+            D. Division.
+            ");
+
+            string selection = Console.ReadLine();
+
+            Operation op = new Operation(val1, val2);
+
+            if (selection == "A")
+            {
+                op.add();
+            }
+            else if (selection == "B")
+            {
+                op.subtract();
+            }
+            else if (selection == "C")
+            {
+                op.multiply();
+            }
+            else if (selection == "D")
+            {
+                op.divide();
+            }
+            else
+            {
+                Console.WriteLine("Wrong selection");
+            }
+
         }
     }
 }
