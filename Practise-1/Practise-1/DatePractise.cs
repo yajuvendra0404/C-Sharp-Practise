8a7e6114cea98428e14716fd502aa92ec4747754
Yajuvendra-Thakur <Yajuvendra.Thakur@flemingcollege.ca>
2026-03-17 17:35:14 -0400

    date practise

---
+++ Practise-1/Practise-1/DatePractise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_1
+{
+    internal class DatePractise
+    {
+        internal static void main( string[] arg) { 
+                   DateTime dt = DateTime.Now;
+            Console.WriteLine("date {0}", dt);
+        }
+    }
+}
