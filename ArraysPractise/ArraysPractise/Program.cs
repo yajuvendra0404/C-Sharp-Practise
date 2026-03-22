7bbc2e1aa38dbf6f5ae9fbad5d831fdffb8cf43b
Yajuvendra-Thakur <Yajuvendra.Thakur@flemingcollege.ca>
2026-03-22 09:56:38 -0400

    changes

--- ArraysPractise/ArraysPractise/Program.cs
+++ ArraysPractise/ArraysPractise/Program.cs
@@ -10,6 +10,15 @@ namespace ArraysPractise
     {
         static void Main(string[] args)
         {
+            int[,] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } }; // Rectangular array with 2 rows and 4 columns
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
