7e49c130bf050cb4eef36830f47317e70c9729a9
Yajuvendra-Thakur <yajuvendra0404@gmail.com>
2026-03-22 18:32:48 -0400

    getter setter practise

---
+++ GetSetPractise/GetSetPractise/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetPractise
+{
+    internal class Car
+    {
+        private int _makeYear;
+        private string _name;
+        private string _engine;
+        private string _type;
+
+        internal int MakeYear {
+            get { return _makeYear; }
+            set { _makeYear = value;  }
+        }
+        internal string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        internal string Engine
+        {
+            get { return _engine; }
+            set { _engine = value; }
+        }
+        internal string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        internal string GetCar() {
+            
+            return $"I am buying a {_makeYear} model {_name}  {_type} with {_engine} engine.";
+        }
+    }
+}
---
+++ GetSetPractise/GetSetPractise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car obj = new Car();
+            obj.MakeYear = 2001;
+            obj.Engine = "v8";
+            obj.Type = "Sedan";
+            obj.Name = "Mazda";
+            string car = obj.GetCar();
+            Console.WriteLine(car);
+        }
+    }
+}
